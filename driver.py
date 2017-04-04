23e95eb29ec2102f279a702e6fedb79420afa560
Brian Clee <bpclee2011@gmail.com>
2017-04-04 13:50:08 -0400

    added resampling and 3D plotting (warning: slow!)

--- driver.py
+++ driver.py
@@ -12,7 +12,7 @@ INPUT_FOLDER = 'sample_images/'
 patients = os.listdir(INPUT_FOLDER)
 patients.sort()
 
-# load the scans
+# load the scans by file path
 def load_scan(path):
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
@@ -26,6 +26,8 @@ def load_scan(path):
 
     return slices
 
+# by default patient dicom files arent returned in hounsfield unit (hu) format
+# this function takes a dicom slice's pixel array and converts to HU
 def get_pixels_hu(slices):
     image = np.stack([s.pixel_array for s in slices])
     # Convert to int16 (from sometimes int16),
@@ -50,13 +52,56 @@ def get_pixels_hu(slices):
 
     return np.array(image, dtype=np.int16)
 
+# different dicom scans can have different spacing, this function attempts to resample
+# to a standard spacing scale
+def resample(image, scan, new_spacing=[1, 1, 1]):
+    # Determine current pixel spacing
+    spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+
+    return image, new_spacing
+
+# 3D plot with matplotlib
+def plot_3d(image, threshold=-300):
+    # Position the scan upright,
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2, 1, 0)
+
+    verts, faces = measure.marching_cubes_classic(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
 first_patient = load_scan(INPUT_FOLDER + patients[1])
 first_patient_pixels = get_pixels_hu(first_patient)
-plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
+pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1,1,1])
+
+plt.hist(pix_resampled.flatten(), bins=80, color='c')
 plt.xlabel("Hounsfield Units (HU)")
 plt.ylabel("Frequency")
 plt.show()
 
 # Show some slice in the middle
-plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
+plt.imshow(pix_resampled[80], cmap=plt.cm.gray)
 plt.show()
+
+plot_3d(pix_resampled, 400)
